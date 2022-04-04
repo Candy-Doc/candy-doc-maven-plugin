d66dc764bb1843240b9db99e152cb3a17e1e0b32
Charles Tacquet <ctacquet@outlook.fr>
2022-04-04 10:43:15 +0200

    Add class params to ConceptFinder

--- src/main/java/io/candydoc/domain/extractor/ConceptFinder.java
+++ src/main/java/io/candydoc/domain/extractor/ConceptFinder.java
@@ -1,8 +1,8 @@
 package io.candydoc.domain.extractor;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
 
 public interface ConceptFinder {
-  Set<Class<?>> findConcepts(String packageToScan);
-
+  Set<Class<?>> findConcepts(String packageToScan, Class<? extends Annotation> conceptClass);
 }
--- src/main/java/io/candydoc/domain/extractor/DomainEventExtractor.java
+++ src/main/java/io/candydoc/domain/extractor/DomainEventExtractor.java
@@ -3,33 +3,33 @@ package io.candydoc.domain.extractor;
 import io.candydoc.domain.command.ExtractDomainEvents;
 import io.candydoc.domain.events.DomainEvent;
 import io.candydoc.domain.events.DomainEventFound;
-import io.candydoc.domain.model.DDDConcept;
-import io.candydoc.domain.model.DDDConceptRepository;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reflections8.Reflections;
 
 @Slf4j
 @RequiredArgsConstructor
 public class DomainEventExtractor implements Extractor<ExtractDomainEvents> {
 
-  private final DDDConceptFinder DDDConceptFinder;
+  private final ConceptFinder conceptFinder;
 
   @Override
   public List<DomainEvent> extract(ExtractDomainEvents command) {
-    Set<DDDConcept> domainEventClasses =
-        DDDConceptFinder.findDomainEvents(command.getPackageToScan());
-    DDDConceptRepository.getInstance().addDDDConcepts(domainEventClasses);
+    Set<Class<?>> domainEventClasses = conceptFinder.findConcepts(command.getPackageToScan(), io.candydoc.domain.annotations.DomainEvent.class);
     log.info("Domain events found in {}: {}", command.getPackageToScan(), domainEventClasses);
     return domainEventClasses.stream()
         .map(
             domainEvent ->
                 DomainEventFound.builder()
-                    .description(domainEvent.getDescription())
-                    .name(domainEvent.getName())
-                    .className(domainEvent.getCanonicalName())
+                    .description(
+                        domainEvent
+                            .getAnnotation(io.candydoc.domain.annotations.DomainEvent.class)
+                            .description())
+                    .name(domainEvent.getSimpleName())
+                    .className(domainEvent.getName())
                     .packageName(domainEvent.getPackageName())
                     .boundedContext(command.getPackageToScan())
                     .build())
