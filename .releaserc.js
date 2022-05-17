b36ead5aaf126fa122a3eae8c884a3195cf5341f
Florian Louvet <flouvet@ippon.fr>
2022-05-17 14:30:22 +0200

    :construction_worker: Add custom config to semantic to implement our vision of the versioning

---
+++ .releaserc.js
@@ -0,0 +1,73 @@
+const {promisify} = require('util')
+const dateFormat = require('dateformat')
+const path = require("path");
+const readFileAsync = promisify(require('fs').readFile)
+const TEMPLATE_DIR = path.join('.github/', 'templates/')
+// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
+
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
+const commitTemplate = readFileAsync(
+    path.join(TEMPLATE_DIR, 'commit-template.hbs'))
+
+module.exports = {
+  branches: [
+    'main'
+  ],
+  plugins: [
+    [
+      'semantic-release-gitmoji', {
+      releaseRules: {
+        major: [
+          ':boom:',
+          ':candy:'
+        ],
+        minor: [
+          ':sparkles:',
+          ':zap:',
+          ':lipstick:',
+          ':recycle:'
+        ],
+        patch: [
+          ':bug:',
+          ':arrow_down:',
+          ':arrow_up:',
+          ':rotating_light:'
+        ]
+      },
+      releaseNotes: {
+        template,
+        partials: {commitTemplate},
+        helpers: {
+          datetime: function (format = 'UTC:yyyy-mm-dd') {
+            return dateFormat(new Date(), format)
+          }
+        },
+        issueResolution: {
+          template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+          baseUrl: 'https://github.com',
+          source: 'github.com'
+        }
+      }
+    }
+    ],
+    '@semantic-release/github',
+    '@semantic-release/changelog',
+    {
+      changelogFile: 'CHANGELOG.md',
+      changelogTitle: '# Changelog'
+    },
+    '@conveyal/maven-semantic-release',
+    '@semantic-release/git',
+    {
+      assets: [
+        'CHANGELOG.md',
+        'pom.xml'
+      ],
+      message: ':bookmark: chore(release): ${nextRelease.gitTag} \n\n${nextRelease.notes}'
+    }
+  ],
+  extends: [
+    'semantic-release-config-gitmoji'
+  ]
+}
